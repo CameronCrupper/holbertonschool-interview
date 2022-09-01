d33bd531e23947e2831ae67403444a76b0f376b4
Cameron Crupper <guitardude20795@yahoo.com>
2022-09-01 15:42:49 -0500

    lockboxes

---
+++ 0-lockboxes.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+Lockboxes
+"""
+
+
+def canUnlockAll(boxes):
+    """
+    loops thru boxes and checks if they can be unlocked
+    """
+    keyset = {0}
+
+    for key in boxes[0]:
+        if (0 <= key < len(boxes)) and key not in keyset:
+            boxes[0].extend(boxes[key])
+            keyset.add(key)
+
+    # print(keyset)
+    return len(keyset) == len(boxes)
