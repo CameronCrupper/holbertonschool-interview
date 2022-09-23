7278575e099e39e8eaaa74515ae23991c7ef56b4
Cameron Crupper <guitardude20795@yahoo.com>
2022-09-22 21:50:04 -0500

    min op

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,35 @@
+#/usr/bin/env python3
+"""
+Minimum operations
+"""
+
+
+import sys
+
+
+def minOperations(n):
+    """
+    calculates fewest number of operations needed to result
+    in exactly n H characters in the file
+    """
+    dp = [sys.maxsize] * (n + 1)
+
+    dp[1] = 0
+
+    for i in range(2, n + 1):
+        if i % 2 == 0:
+            x = dp[i // 2]
+            if x + 1 < dp[i]:
+                dp[i] = x + 1
+
+        if i % 3 == 0:
+            x = dp[i // 3]
+            if x + 1 < dp[i]:
+                dp[i] = x + 1
+
+        x = dp[i - 1]
+        if x + 1 < dp[i]:
+            dp[i] = x + 1
+
+    return dp[n]
+
