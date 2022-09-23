ac702a740825d0b1afcd24b1708b761dfd6bb08e
Cameron Crupper <guitardude20795@yahoo.com>
2022-09-22 22:16:34 -0500

    final solution

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -4,32 +4,22 @@ Minimum operations
 """
 
 
-import sys
-
-
 def minOperations(n):
     """
     calculates fewest number of operations needed to result
     in exactly n H characters in the file
     """
-    dp = [sys.maxsize] * (n + 1)
+    if n <= 1:
+        return 0
+    number = n
+    divide = 2
+    numOfOp = 0
 
-    dp[1] = 0
+    while number > 1:
+        if number % divide == 0:
+            number = number / divide
+            numOfOp = numOfOp + divide
+        else:
+            divide += 1
 
-    for i in range(2, n + 1):
-        if i % 2 == 0:
-            x = dp[i // 2]
-            if x + 1 < dp[i]:
-                dp[i] = x + 1
-
-        if i % 3 == 0:
-            x = dp[i // 3]
-            if x + 1 < dp[i]:
-                dp[i] = x + 1
-
-        x = dp[i - 1]
-        if x + 1 < dp[i]:
-            dp[i] = x + 1
-
-    return dp[n]
-
+    return numOfOp
