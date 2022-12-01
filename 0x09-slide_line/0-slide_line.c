57ff79a9f006eb0f49cf0c85568c3cb87adb0886
Cameron Crupper <guitardude20795@yahoo.com>
2022-12-01 16:31:45 -0600

    fixing some lines

--- 0x09-slide_line/0-slide_line.c
+++ 0x09-slide_line/0-slide_line.c
@@ -1,16 +1,17 @@
 #include "slide_line.h"
 
+
 /**
  * slide_line - slide & merge it to the left or to the right
- * @line: points to an array of integers
- * @size: Size of the array
- * @direction: Direction in which to slide the array
- * Return\ return 1 upon success, or 0 upon failure
+ * @line: points to an array of int
+ * @size: Size of the array being pointed to
+ * @direction: Direction to slide the array
+ * Return: return 1 upon success, or 0 upon failure
  */
 
 int slide_line(int *line, size_t size, int direction)
 {
-	size_t aux, mark = 0, p1 = 0, p2 = 1, t1, t2;
+	size_t aux, x = 0, p1 = 0, p2 = 1, t1, t2;
 
 	if (direction != SLIDE_LEFT && direction != SLIDE_RIGHT)
 		return (0);
@@ -19,7 +20,7 @@ int slide_line(int *line, size_t size, int direction)
 
 	for (t1 = 0; t1 < size; t1++)
 	{       aux = p2;
-		mark = 0;
+		x = 0;
 		for (t2 = t1 + 1; t2 < size; t2++)
 		{
 			if (line[p1] != 0 && line[p2] == line[p1])
@@ -31,7 +32,7 @@ int slide_line(int *line, size_t size, int direction)
 			if (line[p1] == 0 && line[p2] != 0)
 			{	line[p1] = line[p2];
 				line[p2] = 0;
-				mark = 1;
+				x = 1;
 				p2 = aux;
 				t1--;
 				break;
@@ -43,10 +44,9 @@ int slide_line(int *line, size_t size, int direction)
 		}
 		if (line[p1] == 0)
 			break;
-		if (mark == 0)
+		if (x == 0)
 		{	p1 = (direction == SLIDE_LEFT) ? p1 + 1 : p1;
-
-			p1 = (direction == SLIDE_RIGHT) ? p1 - 2 : p1;
+			p1 = (direction == SLIDE_RIGHT) ? p1 - 1 : p1;
 			p2 = (direction == SLIDE_LEFT) ? p1 + 1 : p2;
 			p2 = (direction == SLIDE_RIGHT) ? p1 - 1 : p2; }
 	}
