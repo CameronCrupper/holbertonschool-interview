559a583fa66a0a76f661391a1e6d6fcc70e82a70
Cameron Crupper <guitardude20795@yahoo.com>
2022-12-08 14:33:57 -0600

    betty

--- 0x0A-menger/0-menger.c
+++ 0x0A-menger/0-menger.c
@@ -9,33 +9,33 @@
 
 void menger(int level)
 {
-  int axisX, axisY, d, dim = 1;
+int axisX, axisY, d, dim = 1;
 
-  if (level >= 0)
-  {
-    for (axisX = 0; axisX < level; axisX++)
-    {
-      dim *= 3;
-    }
-    for (axisX = 0; axisX < dim; axisX++)
-    {
-      for (axisY = 0; axisY < dim; axisY++)
-      {
-        for (d = dim / 3; d > 0; d /= 3)
-        {
-          if ((axisX % (d * 3)) / d == 1 && (axisY % (d * 3)) / d == 1)
-          break;
-        }
-        if (d)
-        {
-          printf(" ");
-        }
-        else
-        {
-          printf("#");
-        }
-      }
-    printf("\n");
-    }
-  }
+if (level >= 0)
+{
+for (axisX = 0; axisX < level; axisX++)
+{
+dim *= 3;
+}
+for (axisX = 0; axisX < dim; axisX++)
+{
+for (axisY = 0; axisY < dim; axisY++)
+{
+for (d = dim / 3; d > 0; d /= 3)
+{
+if ((axisX % (d * 3)) / d == 1 && (axisY % (d * 3)) / d == 1)
+break;
+}
+if (d)
+{
+printf(" ");
+}
+else
+{
+printf("#");
+}
+}
+printf("\n");
+}
+}
 }
