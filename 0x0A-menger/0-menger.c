6ffe5a573cea47c557e14430edda6b7541218c6d
Cameron Crupper <guitardude20795@yahoo.com>
2022-12-08 14:28:34 -0600

    fun sponge

---
+++ 0x0A-menger/0-menger.c
@@ -0,0 +1,41 @@
+#include "menger.h"
+#include <stdio.h>
+
+/**
+ * menger - Draws 2D menger sponge
+ * @level: depth of sponge to print
+ * Return: none
+ */
+
+void menger(int level)
+{
+    int axisX, axisY, d, dim = 1;
+
+    if (level >= 0)
+    {
+        for (axisX = 0; axisX < level; axisX++)
+        {
+            dim *= 3;
+        }
+        for (axisX = 0; axisX < dim; axisX++)
+        {
+            for (axisY = 0; axisY < dim; axisY++)
+            {
+                for (d = dim / 3; d > 0; d /= 3)
+                {
+                    if ((axisX % (d * 3)) / d == 1 && (axisY % (d * 3)) / d == 1)
+                    break;
+                }
+                if (d)
+                {
+                    printf(" ");
+                }
+                else
+                {
+                    printf("#");
+                }
+            }
+        printf("\n");
+        }
+    }
+}
