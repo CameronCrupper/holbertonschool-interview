09cfd2b5eebca40421edcd0b2147938874871e4d
CameronCrupper <guitardude20795@yahoo.com>
2023-03-14 23:21:15 -0500

    test

--- 0x1C-primegame/0-prime_game.py
+++ 0x1C-primegame/0-prime_game.py
@@ -1,20 +1,4 @@
 #!/usr/bin/python3
-"""
-Maria and Ben are playing a game. Given a set of
-consecutive integers starting from 1 up to and
-including n, they take turns choosing a prime number
-from the set and removing that number and its
-multiples from the set. The player that cannot
-make a move loses the game.
-"""
-
-
-# def is_prime(x, nums):
-#     if nums > 1:
-#         for x in range(2, int(num/2)+1):
-#             if (nums % x) == 0:
-#                 return False
-#     return True
 
 def isWinner(x, nums):
     """
@@ -31,3 +15,10 @@ def isWinner(x, nums):
         if nums[i] in nums[i+1:n]:
             nums.remove(nums[i])
     return isWinner(x-1, nums)
+
+# def is_prime(x, nums):
+#     if nums > 1:
+#         for x in range(2, int(num/2)+1):
+#             if (nums % x) == 0:
+#                 return False
+#     return True
